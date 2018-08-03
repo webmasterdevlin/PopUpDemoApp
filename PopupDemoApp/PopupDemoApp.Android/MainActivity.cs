87ac2d2251521758bd767bfb0702d14ae3452da3
webmasterdevlin <webmasterdevlin@gmail.com>
2018-08-03 20:23:18 +0800

    Updated Rg.Plugins.Popup and Xamarin Forms.

--- PopupDemoApp/PopupDemoApp.Android/MainActivity.cs
+++ PopupDemoApp/PopupDemoApp.Android/MainActivity.cs
@@ -18,7 +18,7 @@ namespace PopupDemoApp.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
+            Rg.Plugins.Popup.Popup.Init(this, bundle);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
