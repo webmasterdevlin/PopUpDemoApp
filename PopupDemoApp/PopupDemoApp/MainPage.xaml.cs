7dfabd220e252501fde851e933daa47929e0699e
webmasterdevlin <webmasterdevlin@gmail.com>
2018-08-03 20:05:38 +0800

    Add project files.

---
+++ PopupDemoApp/PopupDemoApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms;
+
+namespace PopupDemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPopupTask(object sender, EventArgs e)
+        {
+            PopupNavigation.PushAsync(new PopupNewTaskView());
+        }
+    }
+}
---
+++ PopupDemoApp/PopupDemoApp/PopupNewTaskView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopupDemoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopupNewTaskView
+    {
+        public PopupNewTaskView()
+        {
+            InitializeComponent();
+        }
+
+        private void TaskEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TaskEntry.Text))
+                TaskButton.IsEnabled = true;
+            else if (string.IsNullOrEmpty(TaskEntry.Text))
+                TaskButton.IsEnabled = false;
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.PopAsync();
+        }
+    }
+}
